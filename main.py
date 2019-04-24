73ea2c17d45684a36a6fd4755b2009a8291e3daa
timbaev <hometim55@gmail.com>
2019-04-24 17:03:30 +0300

    [WIP] Реализовать не менее 5-ти собственных признаков (можно больше), улучшающих результаты классификатора, полученные с использованием признака tf-idf. Оценить поочередно качество классификатора, обученного на tf-idf и каждом из признаков

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import ast
 import logging
 import string
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -9,6 +10,7 @@ from nltk.corpus import stopwords
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import ExtraTreesClassifier
 from pymystem3 import Mystem
+from gensim.models import Word2Vec
 
 CORPUS_PATH = 'data/corpus.txt'
 log = logging.getLogger()
@@ -77,12 +79,22 @@ def tokenize(document):
     return tokens
 
 
-def tokenize_documents(documents):
+def tokenize_documents_extend(documents):
     texts = []
 
     for document in documents:
         w = tokenize(document)
         texts.extend(w)
+
+    return texts
+
+
+def tokenize_documents_append(documents):
+    texts = []
+
+    for document in documents:
+        w = tokenize(document)
+        texts.append(w)
 
     return texts
 
@@ -111,11 +123,39 @@ def setup_logger():
     log.setLevel(logging.INFO)
 
 
+def makeFeatureVec(words, model, num_features):
+    featureVec = np.zeros((num_features,), dtype="float32")
+
+    nwords = 0
+
+    index2word_set = set(model.wv.index2word)
+
+    for word in words:
+        if word in index2word_set:
+            nwords = nwords + 1
+            featureVec = np.add(featureVec, model[word])
+
+    featureVec = np.divide(featureVec, nwords)
+    return featureVec
+
+
+def getAvgFeatureVecs(reviews, model, num_features):
+    counter = 0
+
+    reviewFeatureVecs = np.zeros((len(reviews), num_features), dtype="float32")
+
+    for review in reviews:
+        reviewFeatureVecs[counter] = makeFeatureVec(review, model, num_features)
+        counter = counter + 1
+
+    return reviewFeatureVecs
+
+
 if __name__ == '__main__':
     setup_logger()
 
     reviews, marks = read_data(CORPUS_PATH)
-    pos_reviews, neg_reviews = split_reviews(reviews[:100], marks[:100])
+    pos_reviews, neg_reviews = split_reviews(reviews, marks)
 
     y_pos = [1] * len(pos_reviews)
     y_neg = [0] * len(neg_reviews)
@@ -132,4 +172,76 @@ if __name__ == '__main__':
 
     token_importances = feature_importances(X_train_vect.toarray(), y_train)
 
-    print_most_valuable_features(tokenize_documents(X_train), token_importances)
+    print_most_valuable_features(tokenize_documents_extend(X_train), token_importances)
+
+    # Test Features char ngram
+    vectorizer = TfidfVectorizer(max_features=50000, min_df=5, analyzer='char', ngram_range=(3, 3))
+    X_train_vect_word_ngram = vectorizer.fit_transform(X_train)
+    X_test_vect_word_ngram = vectorizer.transform(X_test)
+
+    train_matrix = np.append(X_train_vect.toarray(), X_train_vect_word_ngram.toarray(), axis=1)
+    test_matrix = np.append(X_test_vect.toarray(), X_test_vect_word_ngram.toarray(), axis=1)
+
+    model = LogisticRegression(solver="lbfgs")
+    model.fit(train_matrix, y_train)
+    print_score_model(model, test_matrix, y_test)
+
+    # Test Features word ngram
+    vectorizer = TfidfVectorizer(max_features=50000, min_df=5, tokenizer=tokenize, analyzer='word', ngram_range=(3, 3))
+    X_train_vect_word_ngram = vectorizer.fit_transform(X_train)
+    X_test_vect_word_ngram = vectorizer.transform(X_test)
+
+    train_matrix = np.append(X_train_vect.toarray(), X_train_vect_word_ngram.toarray(), axis=1)
+    test_matrix = np.append(X_test_vect.toarray(), X_test_vect_word_ngram.toarray(), axis=1)
+
+    model = LogisticRegression(solver="lbfgs")
+    model.fit(train_matrix, y_train)
+    print_score_model(model, test_matrix, y_test)
+
+    # Test Features avg Word2Vec
+    train_sentences = tokenize_documents_append(X_train)
+    word2vec_train_model = Word2Vec(train_sentences)
+    train_avg_vectors = getAvgFeatureVecs(train_sentences, word2vec_train_model, 100)
+
+    test_sentences = tokenize_documents_append(X_test)
+    word2vec_test_model = Word2Vec(test_sentences)
+    test_avg_vectors = getAvgFeatureVecs(test_sentences, word2vec_test_model, 100)
+
+    X_train_bow_vectors = np.append(X_train_vect.toarray(), train_avg_vectors, axis=1)
+    X_test_bow_vectors = np.append(X_test_vect.toarray(), test_avg_vectors, axis=1)
+
+    # Test Feature word count
+    train_sentences = tokenize_documents_append(X_train)
+    train_word_counts = [len(tokens) for tokens in train_sentences]
+
+    test_sentences = tokenize_documents_append(X_test)
+    test_word_counts = [len(tokens) for tokens in test_sentences]
+
+    train_matrix = np.append(X_train_vect.toarray(), train_word_counts, axis=1)
+    test_matrix = np.append(X_test_vect.toarray(), test_word_counts, axis=1)
+
+    model = LogisticRegression(solver="lbfgs")
+    model.fit(train_matrix, y_train)
+    print_score_model(model, test_matrix, y_test)
+
+    # Test Feature characters count
+    train_char_counts = [len(document) for document in X_train]
+    test_char_counts = [len(document) for document in X_test]
+
+    train_matrix = np.append(X_train_vect.toarray(), train_char_counts, axis=1)
+    test_matrix = np.append(X_test_vect.toarray(), test_char_counts, axis=1)
+
+    model = LogisticRegression(solver="lbfgs")
+    model.fit(train_matrix, y_train)
+    print_score_model(model, test_matrix, y_test)
+
+    # Test Feature brackets count
+    train_bracket_counts = [document.count(')') - document.count('(') for document in X_train]
+    test_bracket_counts = [document.count(')') - document.count('(') for document in X_test]
+
+    train_matrix = np.append(X_train_vect.toarray(), train_bracket_counts, axis=1)
+    test_matrix = np.append(X_test_vect.toarray(), test_bracket_counts, axis=1)
+
+    model = LogisticRegression(solver="lbfgs")
+    model.fit(train_matrix, y_train)
+    print_score_model(model, test_matrix, y_test)
