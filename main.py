6d3e382b4f8c91782efd5d60b514641cc6ed230e
timbaev <hometim55@gmail.com>
2019-05-08 16:04:42 +0300

    Refactor and add GridSearch

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import ast
 import logging
 import string
 import numpy as np
+import codecs
 
-from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import precision_recall_fscore_support
 from nltk.corpus import stopwords
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import ExtraTreesClassifier
 from pymystem3 import Mystem
-from gensim.models import Word2Vec
 
 CORPUS_PATH = 'data/corpus.txt'
+POSITIVE_PATH = 'data/positive.txt'
+NEGATIVE_PATH = 'data/negative.txt'
+
 log = logging.getLogger()
 
 POS_MARK = 1
@@ -151,6 +155,100 @@ def getAvgFeatureVecs(reviews, model, num_features):
     return reviewFeatureVecs
 
 
+def read_words(filepath):
+    with codecs.open(filepath, 'r', 'utf-8') as f:
+        words = f.readlines()
+        words = [x.strip() for x in words]
+        return set(words)
+
+
+def sentimental_values(documents, positive_words, negative_words):
+    sentimental_values = []
+
+    for document in documents:
+        sentimental_value = 0
+        for token in document:
+            if token in positive_words:
+                sentimental_value += 1
+            elif token in negative_words:
+                sentimental_value -= 1
+
+        sentimental_values.append(sentimental_value)
+
+    return sentimental_values
+
+
+def fit(x_train, y_train, x_test, y_test):
+    model = LogisticRegression(solver="lbfgs")
+    model.fit(x_train, y_train)
+    print_score_model(model, x_test, y_test)
+
+
+def get_documents_tokens(x_train, x_test):
+    train_tokens = tokenize_documents_append(x_train)
+    test_tokens = tokenize_documents_append(x_test)
+    return train_tokens, test_tokens
+
+
+def get_char_ngram_feature(x_train, x_test):
+    vectorizer = TfidfVectorizer(max_features=50000, min_df=5, analyzer='char', ngram_range=(3, 3))
+    X_train_vect_char_ngram = vectorizer.fit_transform(x_train)
+    X_test_vect_char_ngram = vectorizer.transform(x_test)
+
+    return X_train_vect_char_ngram, X_test_vect_char_ngram
+
+
+def get_word_ngram_feature(x_train, x_test):
+    vectorizer = TfidfVectorizer(max_features=50000, min_df=5, analyzer='word', ngram_range=(3, 3))
+    X_train_vect_word_ngram = vectorizer.fit_transform(x_train)
+    X_test_vect_word_ngram = vectorizer.transform(x_test)
+
+    return X_train_vect_word_ngram, X_test_vect_word_ngram
+
+
+def get_word_count_feature(train_tokens, test_tokens):
+    train_word_counts = [len(tokens) for tokens in train_tokens]
+    train_word_counts = np.reshape(train_word_counts, (-1, 1))
+
+    test_word_counts = [len(tokens) for tokens in test_tokens]
+    test_word_counts = np.reshape(test_word_counts, (-1, 1))
+
+    return train_word_counts, test_word_counts
+
+
+def get_char_count_feature(X_train, X_test):
+    train_char_counts = [len(document) for document in X_train]
+    train_char_counts = np.reshape(train_char_counts, (-1, 1))
+
+    test_char_counts = [len(document) for document in X_test]
+    test_char_counts = np.reshape(test_char_counts, (-1, 1))
+
+    return train_char_counts, test_char_counts
+
+
+def get_bracket_count_feature(X_train, X_test):
+    train_bracket_counts = [document.count(')') - document.count('(') for document in X_train]
+    train_bracket_counts = np.reshape(train_bracket_counts, (-1, 1))
+
+    test_bracket_counts = [document.count(')') - document.count('(') for document in X_test]
+    test_bracket_counts = np.reshape(test_bracket_counts, (-1, 1))
+
+    return train_bracket_counts, test_bracket_counts
+
+
+def get_sentimental_feature(train_tokens, test_tokens):
+    positive_words = read_words(POSITIVE_PATH)
+    negative_words = read_words(NEGATIVE_PATH)
+
+    train_sentimental_values = sentimental_values(train_tokens, positive_words, negative_words)
+    train_sentimental_values = np.reshape(train_sentimental_values, (-1, 1))
+
+    test_sentimental_values = sentimental_values(test_tokens, positive_words, negative_words)
+    test_sentimental_values = np.reshape(test_sentimental_values, (-1, 1))
+
+    return train_sentimental_values, test_sentimental_values
+
+
 if __name__ == '__main__':
     setup_logger()
 
@@ -166,94 +264,84 @@ if __name__ == '__main__':
     X_train_vect = vectorizer.fit_transform(X_train)
     X_test_vect = vectorizer.transform(X_test)
 
-    model = LogisticRegression(solver="lbfgs")
-    model.fit(X_train_vect.toarray(), y_train)
-    print_score_model(model, X_test_vect.toarray(), y_test)
+    fit(X_train_vect.toarray(), y_train, X_test_vect.toarray(), y_test)
 
     token_importances = feature_importances(X_train_vect.toarray(), y_train)
-
     print_most_valuable_features(tokenize_documents_extend(X_train), token_importances)
 
     # Test Features char ngram
-    vectorizer = TfidfVectorizer(max_features=50000, min_df=5, analyzer='char', ngram_range=(3, 3))
-    X_train_vect_word_ngram = vectorizer.fit_transform(X_train)
-    X_test_vect_word_ngram = vectorizer.transform(X_test)
+    X_train_vect_char_ngram, X_test_vect_char_ngram = get_char_ngram_feature(X_train, X_test)
 
-    train_matrix = np.append(X_train_vect.toarray(), X_train_vect_word_ngram.toarray(), axis=1)
-    test_matrix = np.append(X_test_vect.toarray(), X_test_vect_word_ngram.toarray(), axis=1)
+    train_matrix = np.append(X_train_vect.toarray(), X_train_vect_char_ngram.toarray(), axis=1)
+    test_matrix = np.append(X_test_vect.toarray(), X_test_vect_char_ngram.toarray(), axis=1)
 
-    model = LogisticRegression(solver="lbfgs")
-    model.fit(train_matrix, y_train)
-    print_score_model(model, test_matrix, y_test)
+    fit(train_matrix, y_train, test_matrix, y_test)
 
     # Test Features word ngram
-    vectorizer = TfidfVectorizer(max_features=50000, min_df=5, tokenizer=tokenize, analyzer='word', ngram_range=(3, 3))
-    X_train_vect_word_ngram = vectorizer.fit_transform(X_train)
-    X_test_vect_word_ngram = vectorizer.transform(X_test)
+    X_train_vect_word_ngram, X_test_vect_word_ngram = get_word_ngram_feature(X_train, X_test)
 
     train_matrix = np.append(X_train_vect.toarray(), X_train_vect_word_ngram.toarray(), axis=1)
     test_matrix = np.append(X_test_vect.toarray(), X_test_vect_word_ngram.toarray(), axis=1)
 
-    model = LogisticRegression(solver="lbfgs")
-    model.fit(train_matrix, y_train)
-    print_score_model(model, test_matrix, y_test)
-
-    # Test Features avg Word2Vec
-    train_sentences = tokenize_documents_append(X_train)
-    word2vec_train_model = Word2Vec(train_sentences)
-    train_avg_vectors = getAvgFeatureVecs(train_sentences, word2vec_train_model, 100)
-
-    test_sentences = tokenize_documents_append(X_test)
-    word2vec_test_model = Word2Vec(test_sentences)
-    test_avg_vectors = getAvgFeatureVecs(test_sentences, word2vec_test_model, 100)
-
-    X_train_bow_vectors = np.append(X_train_vect.toarray(), train_avg_vectors, axis=1)
-    X_test_bow_vectors = np.append(X_test_vect.toarray(), test_avg_vectors, axis=1)
-
-    model = LogisticRegression(solver="lbfgs")
-    model.fit(X_train_bow_vectors, y_train)
-    print_score_model(model, X_test_bow_vectors, y_test)
+    fit(train_matrix, y_train, test_matrix, y_test)
 
     # Test Feature word count
-    train_sentences = tokenize_documents_append(X_train)
-    train_word_counts = [len(tokens) for tokens in train_sentences]
-    train_word_counts = np.reshape(train_word_counts, (-1, 1))
+    train_tokens, test_tokens = get_documents_tokens(X_train, X_test)
 
-    test_sentences = tokenize_documents_append(X_test)
-    test_word_counts = [len(tokens) for tokens in test_sentences]
-    test_word_counts = np.reshape(test_word_counts, (-1, 1))
+    train_word_counts, test_word_counts = get_word_count_feature(train_tokens, test_tokens)
 
     train_matrix = np.append(X_train_vect.toarray(), train_word_counts, axis=1)
     test_matrix = np.append(X_test_vect.toarray(), test_word_counts, axis=1)
 
-    model = LogisticRegression(solver="lbfgs")
-    model.fit(train_matrix, y_train)
-    print_score_model(model, test_matrix, y_test)
+    fit(train_matrix, y_train, test_matrix, y_test)
 
     # Test Feature characters count
-    train_char_counts = [len(document) for document in X_train]
-    train_char_counts = np.reshape(train_char_counts, (-1, 1))
-
-    test_char_counts = [len(document) for document in X_test]
-    test_char_counts = np.reshape(test_char_counts, (-1, 1))
+    train_char_counts, test_char_counts = get_char_count_feature(X_train, X_test)
 
     train_matrix = np.append(X_train_vect.toarray(), train_char_counts, axis=1)
     test_matrix = np.append(X_test_vect.toarray(), test_char_counts, axis=1)
 
-    model = LogisticRegression(solver="lbfgs")
-    model.fit(train_matrix, y_train)
-    print_score_model(model, test_matrix, y_test)
+    fit(train_matrix, y_train, test_matrix, y_test)
 
     # Test Feature brackets count
-    train_bracket_counts = [document.count(')') - document.count('(') for document in X_train]
-    train_bracket_counts = np.reshape(train_bracket_counts, (-1, 1))
-
-    test_bracket_counts = [document.count(')') - document.count('(') for document in X_test]
-    test_bracket_counts = np.reshape(test_bracket_counts, (-1, 1))
+    train_bracket_counts, test_bracket_counts = get_bracket_count_feature(X_train, X_test)
 
     train_matrix = np.append(X_train_vect.toarray(), train_bracket_counts, axis=1)
     test_matrix = np.append(X_test_vect.toarray(), test_bracket_counts, axis=1)
 
-    model = LogisticRegression(solver="lbfgs")
-    model.fit(train_matrix, y_train)
-    print_score_model(model, test_matrix, y_test)
+    fit(train_matrix, y_train, test_matrix, y_test)
+
+    # Test Feature Sentimental
+    train_sentimental_values, test_sentimental_values = get_sentimental_feature(train_tokens, test_tokens)
+
+    train_matrix = np.append(X_train_vect.toarray(), train_sentimental_values, axis=1)
+    test_matrix = np.append(X_test_vect.toarray(), test_sentimental_values, axis=1)
+
+    fit(train_matrix, y_train, test_matrix, y_test)
+
+    # All futures
+    train_matrix = np.append(X_train_vect.toarray(), X_train_vect_char_ngram.toarray(), axis=1)
+    test_matrix = np.append(X_test_vect.toarray(), X_test_vect_char_ngram.toarray(), axis=1)
+
+    train_matrix = np.append(train_matrix, X_train_vect_word_ngram.toarray(), axis=1)
+    test_matrix = np.append(test_matrix, X_test_vect_word_ngram.toarray(), axis=1)
+
+    train_matrix = np.append(train_matrix, train_word_counts, axis=1)
+    test_matrix = np.append(test_matrix, test_word_counts, axis=1)
+
+    train_matrix = np.append(train_matrix, train_char_counts, axis=1)
+    test_matrix = np.append(test_matrix, test_char_counts, axis=1)
+
+    train_matrix = np.append(train_matrix, train_bracket_counts, axis=1)
+    test_matrix = np.append(test_matrix, test_bracket_counts, axis=1)
+
+    fit(train_matrix, y_train, test_matrix, y_test)
+
+    # GridSearchCV
+    parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 10]}
+    svc = svm.SVC(gamma="scale")
+    clf = GridSearchCV(svc, parameters, cv=5)
+    clf.fit(train_matrix, y_train)
+    print(clf.best_estimator_.feature_importance())
+
+
